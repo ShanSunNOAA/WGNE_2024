ded90ddeb8222a1d4bfdfa5c7d7b428709c520b1
Shan.Sun <shan.sun@noaa.gov>
2024-04-08 04:51:17 +0000

    -- Updaing scripts; -- Saving wgne data under model_data.

--- scripts/fig11b_bar_nhx_shx_sep.m
+++ scripts/fig11b_bar_nhx_shx_sep.m
@@ -71,23 +71,23 @@ for nplot=1:6
 
 if (nplot==1) i1=i0; j1=j0-hgt;        titl0='NHX T2m'; end
 if (nplot==2) i1=i0+w+dw;              titl0='SHX T2m'; end
-if (nplot==3) i1=i0; j1=j0-hgt*2-dh;   titl0='NHX Precip'; end
-if (nplot==4) i1=i0+w+dw;              titl0='SHX Precip'; end
-if (nplot==5) i1=i0; j1=j0-hgt*3-dh*2; titl0='NHX H500'; end
-if (nplot==6) i1=i0+w+dw;              titl0='SHX H500'; end
+if (nplot==3) i1=i0; j1=j0-hgt*2-dh;   titl0='NHX H500'; end
+if (nplot==4) i1=i0+w+dw;              titl0='SHX H500'; end
+if (nplot==5) i1=i0; j1=j0-hgt*3-dh*2; titl0='NHX Precip'; end
+if (nplot==6) i1=i0+w+dw;              titl0='SHX Precip'; end
 axes('position',[i1,j1,w,hgt])
 if (nplot<=2) 
 y(1:5,1)=t01(nplot,1:5);
 y(1:5,2)=t02(nplot,1:5);
 y(1:5,3)=t03(nplot,1:5); 
 elseif  (nplot==3) || (nplot==4)  
-y(1:5,1)=p01(nplot-2,1:5);
-y(1:5,2)=p02(nplot-2,1:5);
-y(1:5,3)=p03(nplot-2,1:5); 
+y(1:5,1)=h01(nplot-2,1:5);
+y(1:5,2)=h02(nplot-2,1:5);
+y(1:5,3)=h03(nplot-2,1:5); 
 elseif  (nplot==5) || (nplot==6)  
-y(1:5,1)=h01(nplot-4,1:5);
-y(1:5,2)=h02(nplot-4,1:5);
-y(1:5,3)=h03(nplot-4,1:5); end
+y(1:5,1)=p01(nplot-4,1:5);
+y(1:5,2)=p02(nplot-4,1:5);
+y(1:5,3)=p03(nplot-4,1:5); end
 
  b=bar(x1,y);
  ylow=.0; yhigh=1.;
@@ -96,7 +96,7 @@ y(1:5,3)=h03(nplot-4,1:5); end
  b(2).FaceColor = 'b';
  b(3).FaceColor = 'r';
  if (nplot < 5) set(gca,'xticklabel',[]); end
- if (nplot == 1) legend('NoAer','ClimAer','ProgAer'); end
+ if (nplot == 6) legend('NoAer','ClimAer','ProgAer'); end
  grid
  title (titl0,'Fontsize',fontl,'Fontweight','normal');
 
@@ -106,5 +106,8 @@ axes('position',[0.35 j0+.01 0.3 0.01])
 title ('(b) ACC Sept 2003-2019','Fontsize',9,'Fontweight','normal');
 axis off
 
+%  legend('GPCP','model aer clim','time-vary aer','location','SW')
+%  title('Global Precipitation May (mm/day) Week 1','fontsize',8)
+%
 print -djpeg  fig11b_bar_nhx_shx_sep.jpg
 print -depsc2 fig11b_bar_nhx_shx_sep.eps
